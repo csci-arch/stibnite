37cd2205962a748fd5e8ebf2bb909c3d1da745de
tolgailtuzer <tolgailtuzer@hotmail.com>
2021-02-08 17:43:57 +0300

    Update for file_operations, documentation_manager - Added read_file_structure function for file_operations - Updated documentation_manager init method

--- stibium/documentation_manager.py
+++ stibium/documentation_manager.py
@@ -1,14 +1,22 @@
-from stibium import file_operations, docstring_styler
+from stibium import file_operations, docstring_styler, constants
+import os
 
 
 class DocumentationManager:
-    def __init__(self, config):
+    """
+        This class includes main documentation operations
+    """
+    def __init__(self, source_path, output_path, os_name):
+        self.package_name = source_path.split(constants.separator_dict[os_name])[-1]
+        self.package_path = os.path.abspath(source_path)
+        self.documentation_name = output_path.split(constants.separator_dict[os_name])[-1]
+        self.documentation_path = os.path.join(os.path.abspath(output_path), "docs")
+        self.os_name = os_name
+        self.file_operator = file_operations.FileOperations()
         self.file_structure = self.get_styled_structure(self.get_file_structure())
-        self.config = config
-        self.file_operator = file_operations.FileOperations("config")
 
     def get_file_structure(self):
-        return self.file_operator.read_file_structure()
+        return self.file_operator.read_file_structure(self.package_path, self.os_name)
 
     def get_styled_structure(self, file_structure):
         return docstring_styler.DocstringStyler('style').get_styled_structure(file_structure)
--- stibium/file_operations.py
+++ stibium/file_operations.py
@@ -1,17 +1,60 @@
+import glob
+import os
+from stibium import constants
+
+
 class FileType:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.classes = []
+        self.functions = []
+        self.folders = None
 
 
 class FolderType:
-    pass
+    def __init__(self, name, parent=None):
+        self.name = name
+        self.folders = {}
+        self.files = {}
+
+    def get_element(self, name):
+        if name in self.folders:
+            return self.folders[name]
+        elif name in self.files:
+            return self.files[name]
+        else:
+            return None
+
+    def add_folder(self, folder):
+        self.folders[folder.name] = folder
+
+    def add_file(self, file):
+        self.files[file.name] = file
 
 
 class FileOperations:
-    def __init__(self, config):
-        self.config = config
+    @staticmethod
+    def read_file_structure(package_path, os_name):
+        separator = constants.separator_dict[os_name]
+        ignored_prefix = "__"
+        root_path = os.path.dirname(package_path)
+        root = FolderType('root')
+        f_list_glob = glob.glob(f'{package_path}{separator}**', recursive=True)
+        list_glob = [path for path in f_list_glob if f'{separator}{ignored_prefix}' not in path and os.path.isfile(path) and path[-3:] == ".py"]
 
-    def read_file_structure(self):
-        pass
+        for element in list_glob:
+            current_node = root
+            path_parts = element[len(package_path) + 1:].split(separator)
+            for idx in range(len(path_parts)-1):
+                if current_node.get_element(path_parts[idx]) is not None:
+                    current_node = current_node.get_element(path_parts[idx])
+                else:
+                    current_node.add_folder(FolderType(path_parts[idx]))
+                    current_node = current_node.get_element(path_parts[idx])
+            current_node.add_file(FileType(path_parts[-1]))
 
+        return root
+
+    @staticmethod
     def write_file_structure(self, file_structure):
         pass
