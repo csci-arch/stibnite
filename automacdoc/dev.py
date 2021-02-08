974c15954d8ccdc89bd68455ae6b025f3c94433a
İzzet Tunç <izzet.tunc@univera.com.tr>
2021-02-08 10:36:34 +0300

    Added dev environment

---
+++ automacdoc/dev.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import os
+from time import sleep
+from subprocess import call, Popen
+import webbrowser
+from automacdoc import write_doc
+
+if __name__ == "__main__":
+    source_path = r"C:\My Files\stibium_input\FortuneTellerServer\src"
+    output_path = r"C:\My Files\stibium_output\output"
+
+    write_doc(source_path, output_path)
+
+    sleep(1)
+
+    os.chdir(output_path)
+
+    call(["mkdocs", "build", "--clean"])
+    Popen(["mkdocs", "serve"])
+
+    sleep(2)
+
+    webbrowser.open("http://127.0.0.1:8000/")
